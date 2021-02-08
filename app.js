261b8a7be34a311457c69c548676ea7f336ac293
Sharmin Akter <sharminshathi53@gmail.com>
2021-02-08 23:44:35 +0600

    third commit

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 const inputFoodName = () => {
     const searchMeal = document.getElementById('food-name').value;
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeal}`
@@ -8,36 +6,25 @@ const inputFoodName = () => {
         .then(res => res.json())
         .then(data => displayFood(data.meals));
 }
+const searchIngredient = () => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meals.strIngredient}`
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayFood(data.meals.strIngredient));
+}
 const displayFood = food => {
     const foodContainer = document.getElementById('food-container');
     food.forEach(foodName => {
         food.map(foodNumber => {
             const foodDiv = document.createElement('div');
             foodDiv.className = "col-md-4 foodSection";
-            foodDiv.innerHTML = ` 
-                       
-            <img class="foodImg" src="${foodNumber.strMealThumb}">
+            foodDiv.innerHTML = `          
+            <img onclick = "searchIngredient()" class="foodImg" src="${foodNumber.strMealThumb}">
             <h3 class="foodName" >${foodNumber.strMeal}</h3>
             </div>
             `;
             foodContainer.appendChild(foodDiv);
-            const foodIngredient = ingredient => {
-            const ingredientContainer = document.getElementById(touchPart);
-            food.forEach(ingredient =>{
-                food.map(ingredientNumber =>{
-                    ingredientNumber.forEach(ingredientSerial =>{
-                    const ingredientDiv = document.createElement('div');
-                    ingredientDiv.className = "col-md-4 foodSection";
-                    ingredientDiv.innerHTML = `
-                    <h3 class="ingredientName">${ingredientSerial.strIngredient} </h3>
-                    `
-                })
-                })
-            })
-            }
         })
-    });
-
-
+    })
 }
-
